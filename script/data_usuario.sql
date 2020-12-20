18dde13a91de92a2fda8f4b591c95c3045c1bc49
Tu nombre <you@example.com>
2020-12-20 15:50:15 -0500

    Update profile

--- script/data_usuario.sql
+++ script/data_usuario.sql
@@ -5,4 +5,10 @@ INSERT INTO roles (nombre) VALUES ("Administrador");
 
 INSERT INTO usuarios (roles_id, email, password, nombres, apellidos, estado) 
        VALUES (1,'usuario01@tecsup.edu.pe','nose','usuario01','Tecsup',1 );
+
+INSERT INTO roles (nombre) VALUES ("Ventas");
+
+INSERT INTO usuarios (roles_id, email, password, nombres, apellidos, estado) 
+       VALUES (2,'usuario02@tecsup.edu.pe','1234','usuario 02','Tecsup',1 );
+              
        
