cf04c568d26c6abdf2e7802ee0071cac7632d9e4
Tu nombre <you@example.com>
2020-12-20 16:08:59 -0500

    Update security

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -53,6 +54,34 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 			throws Exception{ 		
 		auth.userDetailsService(userDetailsService);
 	}
+	
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		
+		http
+			// Configure authorizations
+			// .authorizeRequests()
+			// .antMatchers("/" /*, "/**"*/).permitAll()
+			// .antMatchers("/home/**").authenticated()
+			// .antMatchers("/admin/**").hasAnyAuthority("Administrador")
+			//   .and()
+		// Change login
+			.formLogin()
+			.loginPage("/login")
+			.loginProcessingUrl("/authenticate")
+			.defaultSuccessUrl("/")
+			.failureUrl("/login?error")
+			.usernameParameter("username")
+			.passwordParameter("password")
+		  .and()
+		 // Change logout
+			 .logout()
+			 .logoutUrl("/logout")
+			 .logoutSuccessUrl("/login")
+		 .and()
+			 .csrf().disable();
+	} 
+
 }
 
 
